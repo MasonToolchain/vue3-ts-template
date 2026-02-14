81166a38ead0f76f88495cb89c61ca3c38394f45
resilience <547471919@qq.com>
2026-02-14 23:53:50 +0800

    perf: 优化 静态资源 统一导入冗余 main.ts 问题

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { installRouter } from '@/router'
 import { installPinia } from './stores'
+import { installAssets } from './plugins/assets'
 
 const app = createApp(App)
 
+installAssets()
 installRouter(app)
 installPinia(app)
 
