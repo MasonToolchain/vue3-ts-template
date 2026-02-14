f17375d1d2612c00ed217701d6df5a37bb92a569
resilience <547471919@qq.com>
2026-02-15 00:18:11 +0800

    chore: 集成原子化方案 并联动设计令牌

--- src/plugins/assets.ts
+++ src/plugins/assets.ts
@@ -1,4 +1,5 @@
 import "@/assets/scss/index.scss"
+import 'virtual:uno.css'
 
 /**
  * 空方法，通过调用该方法，导入上面 import 中的样式和资源
--- src/views/demo.vue
+++ src/views/demo.vue
@@ -1,9 +1,25 @@
 <template>
-  <div class="demo">
+  <!-- Pinia 持久化 -->
+  <div>
     <h2>Pinia 测试</h2>
     <h3>计数器: {{ counter }}</h3>
     <button @click="demoStore.increment()">点击+1</button>
   </div>
+
+  <!-- Unocss -->
+  <div class="demo">Hello world</div>
+  <div class="text-xs text-primary">测试文字</div>
+  <div class="text-sm text-success">测试文字</div>
+  <div class="text-md text-info">测试文字</div>
+  <div class="text-base text-warning">测试文字</div>
+  <div class="text-lg text-danger">测试文字</div>
+  <div class="text-xl text-text-primary">测试文字</div>
+  <div class="text-2xl text-text-regular">测试文字</div>
+  <div class="text-3xl text-text-secondary">测试文字</div>
+
+  <button class="mt-xl px-md py-sm border rounded-base" @click="toggleTheme">
+    切换深色/浅色模式
+  </button>
 </template>
 
 <script setup lang="ts">
@@ -14,4 +30,16 @@ import { useDemoStore } from '@/stores/modules/demo'
 const demoStore = useDemoStore()
 
 const { counter } = storeToRefs(demoStore)
+
+const toggleTheme = () => {
+  const html = document.documentElement
+  const currentTheme = html.getAttribute('data-theme')
+  html.setAttribute('data-theme', currentTheme === 'dark' ? 'light' : 'dark')
+}
 </script>
+
+<style scoped lang="scss">
+.demo {
+  @apply text-xl text-primary bg-success-light-7;
+}
+</style>
