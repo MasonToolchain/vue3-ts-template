13c1f976cdb10a2294e90bd044d41615a73d52f0
resilience <547471919@qq.com>
2026-02-15 09:54:17 +0800

    feat: 文件路由体系的多layouts布局方案集成

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
 import type { App } from 'vue'
 import { routes } from 'vue-router/auto-routes'
+import { setupLayouts } from 'virtual:generated-layouts'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes: setupLayouts(routes as RouteRecordRaw[])
 })
 
 export const installRouter = (app: App) => {
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import UnoCSS from 'unocss/vite'
 import VueRouter from 'unplugin-vue-router/vite'
+import Layouts from 'vite-plugin-vue-layouts'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -20,7 +21,11 @@ export default defineConfig({
     createSvgIconsPlugin({
       iconDirs: [fileURLToPath(new URL('./src/assets/icons', import.meta.url))],
       symbolId: 'icon-[dir]-[name]',
-    })
+    }), 
+    Layouts({
+      layoutsDirs: 'src/layouts',
+      defaultLayout: 'default',
+    }),
   ],
   resolve: {
     alias: {
